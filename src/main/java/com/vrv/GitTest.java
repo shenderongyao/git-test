578fc6af26dfe6b6cad4c9df33cc87228ddf3e2b
shenderongyao <118799076+shenderongyao@users.noreply.github.com>
2022-12-09 16:04:00 +0800

    Update GitTest.java
    five for commit github v5

--- src/main/java/com/vrv/GitTest.java
+++ src/main/java/com/vrv/GitTest.java
@@ -6,6 +6,6 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello github push v4");
-
+        System.out.println("hello github pull v5");
     }
 }
